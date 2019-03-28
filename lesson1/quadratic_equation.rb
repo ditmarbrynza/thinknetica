7d3bd6bea7e6d367c240365fb00628af4f199d5c
ditmarbrynza <ditmarwork@gmail.com>
2019-03-29 00:57:33 +0300

    add puts d

--- lesson1/quadratic_equation.rb
+++ lesson1/quadratic_equation.rb
@@ -10,10 +10,13 @@ d = b**2-4*a*c
 if (d > 0)
     x1 = (-b + Math.sqrt(d))/(2*a) 
     x2 = (-b - Math.sqrt(d))/(2*a) 
+    puts "d = #{d}"
     puts "x1 = #{x1}, x2 = #{x2}"
 elsif (d == 0)
     x1 = -b/(2*a)
+    puts "d = #{d}"
     puts "x1 = x2 = #{x1}"
 elsif(d < 0)
+    puts "d = #{d}"
     puts "Корней нет"
 end
