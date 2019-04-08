3a9c5dcd480b9e60dc65f848aa66e28374685ba4
ditmarbrynza <ditmarwork@gmail.com>
2019-04-09 00:41:34 +0300

    movie4

---
+++ movie4/car.rb
@@ -0,0 +1,24 @@
+class Car
+  attr_accessor :color
+  attr_reader :drivers
+  attr_reader :number
+
+  def initialize(number)
+    @number = number
+    @color = 'white'
+    @drivers = []
+  end
+
+  def beep
+    puts 'beep beep'
+  end
+  
+  def add_driver(driver)
+    @drivers << driver
+  end
+
+  def show_drivers
+    @drivers.each { |driver| puts driver.name }
+  end
+
+end
---
+++ movie4/driver.rb
@@ -0,0 +1,7 @@
+class Driver
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+end
