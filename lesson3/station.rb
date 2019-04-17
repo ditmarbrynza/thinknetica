f5bd12ebf357655bee625795d6034f4a2897a688
ditmarbrynza <ditmarwork@gmail.com>
2019-04-18 00:49:25 +0300

    complite station

--- lesson3/station.rb
+++ lesson3/station.rb
@@ -1,27 +1,52 @@
 class Station
-
+	attr_reader :name
+	# Имеет название, которое указывается при ее создании 
 	def initialize(name)
-		# Имеет название, которое указывается при ее создании 
 		@name = name
 		@trains_on_station = []
 	end
 
 	# Может принимать поезда (по одному за раз)
 	def take_train(train)
-		@trains_on_station << train
+		@trains_on_station << train #объект поезд пишется в массив
 	end
 
-	# Может возвращать список всех поездов на станции, находящиеся в текущий момент
-	def trains_on_station
-		puts @trains_on_station
+	# Может возвращать список всех поездов на станции, которые находятся на ней в текущий момент
+	def trains_at_station
+		puts "Список всех поездов, находящихся на станции в текущий момент"
+		@trains_on_station.each do | train |
+			puts "#{train.number}"
+		end
 	end
 
-	def self
-		puts "#{self.class}"
+	# Может возвращать список поездов на станции по типу (см. ниже): кол-во грузовых, пассажирских
+	def trains_at_station_by_type(type)
+
+		return if ( type != "cargo" ) && ( type != "passenger" )
+
+		quantity = 0
+		train_buf = []
+		puts "Список и количество поездов типа #{type}:"
+		@trains_on_station.each do | train |
+			if train.type == type
+				train_buf << train.number
+				quantity += 1
+			end
+		end
+		
+		puts quantity
+
+		train_buf.each do | train |
+			puts train
+		end
+
+	end
+
+	# Может отправлять поезда (по одному за раз, при этом, поезд удаляется из списка поездов, находящихся на станции).
+	def send_train(train)
+		@trains_on_station.delete(train)
 	end
 
 end
 
-
-# Может возвращать список поездов на станции по типу (см. ниже): кол-во грузовых, пассажирских
-# Может отправлять поезда (по одному за раз, при этом, поезд удаляется из списка поездов, находящихся на станции).
+st0 = Station.new("st0")
