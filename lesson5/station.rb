a42fb84af01aed08664d6b5866bb549fad0c728c
ditmarbrynza <ditmarwork@gmail.com>
2019-05-12 13:17:50 +0300

    starting lesson5

---
+++ lesson5/station.rb
@@ -0,0 +1,26 @@
+class Station
+
+  attr_reader :name, :trains
+
+  def initialize(name)
+    @name = name
+    @trains = []
+  end
+
+  def take_train(train)
+    @trains << train
+  end
+
+  def trains_by_type(type)
+    @trains.select { |train| train.type == type }.size
+  end
+
+  def send_train(train)
+    @trains.delete(train)
+  end
+
+  def to_s
+    name
+  end
+
+end
